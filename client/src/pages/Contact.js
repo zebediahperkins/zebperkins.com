f2874d75bd738d0fb1ae32a6366209776a3c64ad
Zebediah Perkins <zebmperkins@gmail.com>
2021-02-08 18:00:17 -0500

    Update Contact.js

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -5,14 +5,8 @@ export default function Contact() {
         <div>
             <h1>Get in touch with me!</h1>
 
-            <h2>Email (Preferred)</h2>
-            <p>If you email me at <strong>zebmperkins@gmail.com</strong>, I will likely get back to you with a detailed response within an hour.</p>
-
-            <h2>LinkedIn</h2>
-            <p>If you wish to contact me on LinkedIn, you can view my profile <strong><a href='https://www.linkedin.com/in/zebediah-perkins-a6772a1b1/'>here</a></strong>.</p>
-
-            <h2>Text/Voicemail</h2>
-            <p>You can text, or try to call me, at <strong>(508)717-2601</strong>. I will likely not answer this phone during normal working hours, but I will get back to you ASAP.</p>
+            <h2>Email</h2>
+            <p><strong>zebmperkins@gmail.com</strong></p>
         </div>
     );
 }
