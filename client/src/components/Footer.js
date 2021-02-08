d77968ffb02539257e1fc361fffdf6830cba049a
Zebediah Perkins <zebmperkins@gmail.com>
2021-02-08 18:03:52 -0500

    Remove LinkedIn button on Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,7 +5,6 @@ export default function Footer() {
         <footer>
             <ul className='footer-links'>
                 <li><a href='https://github.com/zebediahperkins'><img src='https://i.imgur.com/fKBRIaD.png' alt='github' /></a></li>
-                <li><a href='https://www.linkedin.com/in/zebediah-perkins-a6772a1b1/'><img src='https://i.imgur.com/zCVhD7r.png' alt='linkedin' /></a></li>
                 <li><a href='https://www.npmjs.com/~zebediahperkins'><img src='https://i.imgur.com/Sr9lOAF.png' alt='npmjs' /></a></li>
                 <li><a href='https://stackoverflow.com/users/13578873/zebediah-perkins?tab=profile'><img src='https://i.imgur.com/6wNKc00.png' alt='stackoverflow' /></a></li>
             </ul>
