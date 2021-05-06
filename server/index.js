ce10acc81ad26d8f1be6d7cddb0f3fa3b0b9ac7a
Zebediah Perkins <zebmperkins@gmail.com>
2021-05-05 23:13:43 -0400

    add email api

--- server/index.js
+++ server/index.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const path = require('path');
+const nodemailer = require('nodemailer')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('/test', (req, res) => {
-    res.send("Test");
+app.get('/send-email', (req, res) => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'toddgered89@gmail.com',
+          pass: 'GreenWedding$12'
+        }
+      });
+      
+      const mailOptions = {
+        from: 'toddgered89@gmail.com',
+        to: 'perkinszebediah@gmail.com',
+        subject: req.query.subject,
+        text: req.query.text
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+    res.send("Email sent!");
 });
 
 app.get('*', (req, res) => {
