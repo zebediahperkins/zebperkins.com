a05397a4b1dd7f3d2c9585111994f8f7c4f567b4
Zebediah Perkins <zebmperkins@gmail.com>
2021-05-06 02:42:52 -0400

    add email api

--- server/index.js
+++ server/index.js
@@ -7,36 +7,40 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
+app.post('/post-email', (req, res) => {
+  res.redirect('/send-email?subject=' + req.query.subject + '&text=' + req.query.text);
+});
+
 app.get('/send-email', (req, res) => {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: 'toddgered89@gmail.com',
-          pass: 'GreenWedding$12'
-        }
-      });
-      
-      const mailOptions = {
-        from: 'toddgered89@gmail.com',
-        to: 'perkinszebediah@gmail.com',
-        subject: req.query.subject,
-        text: req.query.text
-      };
-      
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
-    res.send("Email sent!");
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'toddgered89@gmail.com',
+      pass: 'GreenWedding$12'
+    }
+  });
+
+  const mailOptions = {
+    from: 'toddgered89@gmail.com',
+    to: 'perkinszebediah@gmail.com',
+    subject: req.query.subject,
+    text: req.query.text
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+  res.send("Email sent!");
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
+  console.log(`listening on port ${PORT}`);
 });
