701eae727f2f79f502e511f0397f2d5dfe1d14e0
Zebediah Perkins <zebmperkins@gmail.com>
2021-10-07 09:33:50 -0400

    clean up contact page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -3,8 +3,6 @@ import React from 'react';
 export default function Contact() {
     return(
         <div>
-            <h1>Get in touch with me!</h1>
-
             <h2>Email</h2>
             <p><strong>zebmperkins@gmail.com</strong></p>
         </div>
