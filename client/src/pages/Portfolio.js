a173b897adb12a2bdfb5fa1ff5c089f5b71e19c5
Zebediah Perkins <zebmperkins@gmail.com>
2021-10-07 09:36:58 -0400

    fix links for portfolio page

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -5,10 +5,10 @@ export default function Portfolio() {
     return(
         <div>
             <h1>Projects I've worked on:</h1>
-            <Project name='zebperkins.com' imgLink='https://i.imgur.com/drwjAtm.png' redirectLink='https://zebperkins.com' description='My portfolio/informational website' />
-            <Project name='Most Trees' imgLink='https://i.imgur.com/DbfFgSJ.png' redirectLink='https://www.npmjs.com/package/most-trees' description='Small js library for implementing tree structures' />
-            <Project name='Bbuu20 Recoil Handler' imgLink='https://i.imgur.com/nQgDIpZ.png' redirectLink='https://github.com/zebediahperkins/bbuu20-s-recoil-handler' description='A recoil "script" I made for the video game Rust' />
-            <Project name='Bbuu20 Tribot Miner' imgLink='https://i.imgur.com/HU5Q1A2.png' redirectLink='https://github.com/zebediahperkins/bbuu20-s-tribot-miner' description='OSRS mining AI, utilizes the Tribot API' />
+            <Project name='zebperkins.com' imgLink='https://i.imgur.com/drwjAtm.png' redirectLink='https://github.com/zebediahperkins/zebperkins.com' description='My portfolio/informational website' />
+            <Project name='Most Trees' imgLink='https://i.imgur.com/DbfFgSJ.png' redirectLink='https://github.com/zebediahperkins/most-trees' description='Small js library for implementing tree structures' />
+            <Project name='Bbuu20 Recoil Handler' imgLink='https://i.imgur.com/nQgDIpZ.png' redirectLink='https://github.com/zebediahperkins/bbuu20-s-recoil-handler' description='An anti-recoil "script" I made for the video game Rust' />
+            <Project name='bProgressive Miner' imgLink='https://i.imgur.com/HU5Q1A2.png' redirectLink='https://github.com/zebediahperkins/bProgressive-Miner' description='OSRS mining AI, utilizes the Tribot API' />
         </div>
     );
 }
