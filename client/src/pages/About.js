212bf445d9d23fa1e6c4b159d453ba44f78ddd14
Zebediah Perkins <zebmperkins@gmail.com>
2021-10-07 09:33:24 -0400

    clean up about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,7 +4,6 @@ export default function About() {
     return(
         <div>
             <h1>Hey, I'm Zebediah Perkins</h1>
-            <h4>Full Stack Software Engineer/Student</h4>
 
             <h2>Education</h2>
             <p>I'm a third year student at the University of Massachusetts Amherst.</p>
@@ -12,14 +11,14 @@ export default function About() {
             <h2>Skills</h2>
             <p>Projects I've worked on have used the following:</p>
             <ul className='circleList'>
-                <li>React and Node.js</li>
+                <li>React and Node.js/Javascript</li>
                 <li>.NET Framework</li>
-                <li>Unity</li>
+                <li>Python</li>
                 <li>Java</li>
             </ul>
 
             <h2>Work Experience</h2>
-            <p>You can find projects I've worked on in the 'Portfolio' section of this website. I am actively seeking an internship for the summer of 2021.</p>
+            <p>You can find projects I've worked on in the 'Portfolio' section of this website. I am actively seeking an internship.</p>
         </div>
     );
 }
